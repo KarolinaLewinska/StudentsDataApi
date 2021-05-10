abecff2804570d892a32ae8d9891fa7331077086
KarolinaLewinska <lewinskak95@gmail.com>
2021-05-10 14:08:29 +0200

    Final

--- Controllers/StudentsDataController.cs
+++ Controllers/StudentsDataController.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using StudentsDataApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace StudentsDataApi.Controllers
 {
-    [Route("api/dane-studentow-wzr")]
+    [Route("api/dane-studentow")]
     [ApiController]
     public class StudentsDataController : ControllerBase
     {
@@ -22,17 +23,22 @@ namespace StudentsDataApi.Controllers
         {
             try
             {
+                if (_context.Students == null)
+                {
+                  throw new Exception("Nie znaleziono danych w bazie");
+                }
                 return _context.Students.ToList();
             }
-            catch (System.Exception)
+            catch (Exception exc)
             {
-                throw;
+                throw exc;
             }
         }
 
-        [HttpGet("{id}"), Route("dane-studenta/{id}")]
+        [HttpGet("{id}"), Route("student/{id}")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(StudentData))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult<StudentData> GetStudentsData(int id)
         {
             try
@@ -41,58 +47,77 @@ namespace StudentsDataApi.Controllers
 
                 if (Student == null)
                 {
-                    return NotFound("Nieprawidłowe id studenta!");
+                    return NotFound("Nie znaleziono danych studenta o id: " + id);
+                }
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest("Nieprawidłowe dane");
                 }
 
                 return Ok(Student);
             }
-            catch (System.Exception)
+            catch (Exception exc)
             {
-                throw;
+                throw exc;
             }
         }
 
         [HttpPost, Route("dodaj-dane")]
         [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult<StudentData> AddStudentData(StudentData student)
         {
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest("Nieprawidłowe dane");
+                }
+
                 _context.Students.Add(student);
                 _context.SaveChanges();
 
                 return CreatedAtAction(nameof(GetStudentsData), new { id = student.Id }, student);
             }
-            catch (System.Exception)
+            catch (Exception exc)
             {
-                throw;
+                throw exc;
             } 
         }
 
         [HttpPatch("{id}"), Route("zaktualizuj-dane/{id}")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(StudentData))]
         public ActionResult<StudentData> PatchStudentData(int id, StudentData student)
         {
             try
             {
                 if (id != student.Id)
                 {
-                    return BadRequest("Nieprawidłowe id studenta!");
+                    return NotFound("Nie znaleziono danych studenta o id: " + id);
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest("Nieprawidłowe dane");
                 }
 
                 _context.Students.Update(student);
                 _context.SaveChanges();
 
-                return Ok("Zaktualizowano dane studenta!");
+                return Ok("Zaktualizowano dane studenta o id: " + id);
             }
-            catch (System.Exception)
+            catch (Exception exc)
             {
-                throw;
+                throw exc;
             }
         }
 
         [HttpDelete("{id}"), Route("usun-dane/{id}")]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(StudentData))]
         public ActionResult<StudentData> DeleteStudentData(int id)
         {
             try
@@ -101,17 +126,21 @@ namespace StudentsDataApi.Controllers
 
                 if (Student == null)
                 {
-                    return NotFound("Nieprawidłowe id studenta!");
+                    return NotFound("Nie znaleziono danych studenta o id: " + id) ;
+                }
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest("Nieprawidłowe dane");
                 }
 
                 _context.Students.Remove(Student);
                 _context.SaveChanges();
 
-                return Ok("Usunięto dane studenta!");
+                return Ok("Usunięto dane studenta");
             }
-            catch (System.Exception)
+            catch (Exception exc)
             {
-                throw;
+                throw exc;
             }
         }
     }
